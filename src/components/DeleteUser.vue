f67e29c7efda20d01f36b652e5215b641c10d33e
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-03-31 15:17:36 -0400

    Use global mixins

--- src/components/DeleteUser.vue
+++ src/components/DeleteUser.vue
@@ -9,24 +9,13 @@
 </template>
 
 <script>
-import UserAlert from './UserAlert.vue';
+import alertMixin from '../mixins/alert.js';
+import UserAlert from '../components/UserAlert.vue';
 
 export default {
+  mixins: [alertMixin],
   components: {
     UserAlert,
-  },
-  data() {
-    return {
-      alertIsVisible: false,
-    };
-  },
-  methods: {
-    showAlert() {
-      this.alertIsVisible = true;
-    },
-    hideAlert() {
-      this.alertIsVisible = false;
-    },
   },
 };
 </script>
